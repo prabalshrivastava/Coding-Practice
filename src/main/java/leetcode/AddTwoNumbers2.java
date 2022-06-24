2a3e043e17593a46cc10e6d61fdefd17b69e68d7
p-shrivastava <prabal.shrivastava@priceline.com>
2022-06-24 11:50:08 +0530

    Add two numbers using LinkedList

--- src/main/java/leetcode/AddTwoNumbers2.java
+++ src/main/java/leetcode/AddTwoNumbers2.java
@@ -1,23 +1,18 @@
 package leetcode;
 
-public class AddTwoNumbers {
-  // https://leetcode.com/problems/add-two-numbers/
-
+public class AddTwoNumbers2 {
   //  You are given two non-empty linked lists representing two non-negative integers. The digits
   // are stored in reverse order, and each of their nodes contains a single digit. Add the two
   // numbers and return the sum as a linked list.
-  //  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
   //  Example 1:
   //  Input: l1 = [2,4,3], l2 = [5,6,4]
   //  Output: [7,0,8]
   //  Explanation: 342 + 465 = 807.
+  //  
   //  Example 2:
   //
-  //  Input: l1 = [0], l2 = [0]
-  //  Output: [0]
-  //  Example 3:
-  //
   //  Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+  //. 9999999 + 9999 = 10009998
   //  Output: [8,9,9,9,0,0,0,1]
 
   //  Constraints:
@@ -26,21 +21,44 @@ public class AddTwoNumbers {
   //  It is guaranteed that the list represents a number that does not have leading zeros.
   public static void main(String[] args) {
 //    sample1();
-//    sample2();
-    sample3();
+   sample2();
+
   }
-  private static void sample3() {
-    LinkedList2 linkedList1 = new LinkedList2();
-    linkedList1.addNode(0);
-    linkedList1.print();
 
-    LinkedList2 linkedList2 = new LinkedList2();
-    linkedList2.addNode(0);
-    linkedList2.print();
+  public static LinkedList2 addTwoNumbers(LinkedList2 l1, LinkedList2 l2) {
+    //add your code here
 
-    LinkedList2 listNode = addTwoNumbers(linkedList1, linkedList2);
-    System.out.println("---------------");
-    listNode.print();
+    LinkedList2 linkedList = new LinkedList2();
+    int carry=0;
+    ListNode2 list1=l1.head;
+    ListNode2 list2=l2.head;
+    while(list1!=null){
+      if(list2!=null){
+        
+        int sum=(list1.val+list2.val+carry)%10;
+        carry=(list1.val+list2.val)/10;
+        linkedList.addNode(sum);
+        list2=list2.next;
+      }else{
+        if(carry!=0){
+          int sum=(list1.val+carry)%10;
+         carry=(list1.val+carry)/10;
+        linkedList.addNode(sum);
+        }else{
+        linkedList.addNode(list1.val);
+        }
+      }
+      list1=list1.next;
+    }
+    if (carry != 0) {
+      linkedList.addNode(carry);
+    }
+    while(list2!=null){
+      linkedList.addNode(list2.val);
+      list2=list2.next;
+    }
+    
+    return linkedList;
   }
 
   private static void sample2() {
@@ -61,6 +79,7 @@ public class AddTwoNumbers {
     linkedList2.addNode(9);
     linkedList2.print();
 
+    System.out.println("------------------------------------------------------");
     LinkedList2 listNode = addTwoNumbers(linkedList1, linkedList2);
     listNode.print();
   }
@@ -78,52 +97,17 @@ public class AddTwoNumbers {
     linkedList2.addNode(4);
     linkedList2.print();
 
+    System.out.println("------------------------------------------------------");
+
     LinkedList2 listNode = addTwoNumbers(linkedList1, linkedList2);
     listNode.print();
   }
-
-  public static LinkedList2 addTwoNumbers(LinkedList2 l1, LinkedList2 l2) {
-    ListNode2 trav1 = l1.head;
-    ListNode2 trav2 = l2.head;
-
-    double val1 = 0;
-    int i1 = 0;
-    while (trav1 != null) {
-      val1 = val1 + trav1.val * Math.pow(10, i1);
-      i1++;
-      trav1 = trav1.next;
-    }
-
-    double val2 = 0;
-    int i2 = 0;
-    while (trav2 != null) {
-      val2 = val2 + trav2.val * Math.pow(10, i2);
-      i2++;
-      trav2 = trav2.next;
-    }
-
-    System.out.println(val1);
-    System.out.println(val2);
-
-    double outputVal = val1 + val2;
-    System.out.println(outputVal);
-
-    LinkedList2 linkedList = new LinkedList2();
-
-    int currentVal = (int) outputVal;
-    while (currentVal != 0) {
-      int toBeInserted = currentVal % 10;
-      currentVal = currentVal / 10;
-      linkedList.addNode(toBeInserted);
-    }
-    return linkedList;
-  }
 }
 /** Definition for singly-linked list. */
-class LinkedList {
+class LinkedList2 {
   ListNode2 head;
 
-  public LinkedList() {
+  public LinkedList2() {
     head = null;
   }
 
@@ -154,18 +138,18 @@ class LinkedList {
   }
 }
 
-class ListNode {
+class ListNode2 {
 
   int val;
   ListNode2 next;
 
-  ListNode() {}
+  ListNode2() {}
 
-  ListNode(int val) {
+  ListNode2(int val) {
     this.val = val;
   }
 
-  ListNode(int val, ListNode2 next) {
+  ListNode2(int val, ListNode2 next) {
     this.val = val;
     this.next = next;
   }
---
+++ src/main/java/scaler/introductiontoproblemsolving/IsItPerfectNumber.java
@@ -0,0 +1,86 @@
+package scaler.introductiontoproblemsolving;
+
+import java.util.Scanner;
+
+public class IsItPerfectNumber {
+  //  Problem Description
+  //  You are given N positive integers.
+  //  For each given integer A, you have to tell whether it is a perfect number or not.
+  //  Perfect number is a positive integer which is equal to the sum of its proper positive
+  // divisors.
+  //      Problem Constraints
+  // 1 <= N <= 10
+  //      1 <= A <= 106
+  //  Input Format
+  //  First line of the input contains a single integer N.
+  //  Each of the next N lines contains a single integer A.
+  //      Output Format
+  //  In a seperate line, print YES if a given integer is perfect, else print NO.
+  //  Example Input
+  //  Input 1:
+  //      2
+  //      4
+  //      6
+  //  Input 2:
+  //      1
+  //      3
+  //  Example Output
+  //  Output 1:
+  //  NO
+  //      YES
+  //  Output 2:
+  //  NO
+  //  Example Explanation
+  //  Explanation 1:
+  //  For A = 4, the answer is "NO" as sum of its proper divisors = 1 + 2 = 3, is not equal to 4.
+  //  For A = 6, the answer is "YES" as sum of its proper divisors = 1 + 2 + 3 = 6, is equal to 6.
+  //  Explanation 2:
+  //  For A = 3, the answer is "NO" as sum of its proper divisors = 1, is not equal to 3.
+  // 10    10
+  // 81    138613      NO
+  // 84    359664      NO
+  // 94    672448      NO
+  // 50    446486      NO
+  // 39    496         YES
+  // 6    606234      NO
+  // 39    738982      NO
+  // 83    8128        YES
+  // 8    484512      NO
+  // 73    513847      NO
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int noOfInputs = sc.nextInt();
+    if (noOfInputs < 1 || noOfInputs > 10) {
+      return;
+    }
+
+    int[] inputs = new int[noOfInputs];
+    for (int i = 0; i < noOfInputs; i++) {
+      //   Scanner sc1 = new Scanner(System.in);
+      int currentInput = sc.nextInt();
+      if (currentInput >= 1 && currentInput <= 1000000) inputs[i] = currentInput;
+      else i--;
+    }
+    //    // System.out.println(Arrays.toString(inputs));
+
+    for (int i = 0; i < inputs.length; i++) {
+      long n = inputs[i];
+      // start finding the divisors.
+      int sum = 0;
+      for (int j = 1; j * j <= n; j++) {
+        if (n % j == 0) {
+          //          System.out.println(j);
+          sum += j;
+          if (j != n / j && n / j != n) {
+            //            System.out.println(n / j);
+            sum += (n / j);
+          }
+        }
+      }
+      //      System.out.println("sum = " + sum);
+      if (sum == n) System.out.println("YES");
+      else System.out.println("NO");
+    }
+  }
+}
