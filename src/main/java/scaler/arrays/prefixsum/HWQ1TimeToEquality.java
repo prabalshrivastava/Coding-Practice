39d03eb501d4a7ae7b20a3406a2595b5131e3f57
p-shrivastava <prabal.shrivastava@priceline.com>
2022-07-05 11:20:09 +0530

    added new examples

---
+++ src/main/java/scaler/arrays/prefixsum/HWQ1TimeToEquality.java
@@ -0,0 +1,22 @@
+package scaler.arrays.prefixsum;
+
+public class HWQ1TimeToEquality {
+  public int solve(int[] A) {
+    int max = A[0];
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] > max) {
+        max = A[i];
+      }
+    }
+    int count = 0;
+    for (int i = 0; i < A.length; i++) {
+      count = count + (max - A[i]);
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    int[] B = new int[] {2, 4, 1, 3, 2};
+    System.out.println(new HWQ1TimeToEquality().solve(B));
+  }
+}
