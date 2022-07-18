fefba266dcce579f02fe403ba348a0d99f741cca
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-07-18 11:14:28 +0530

    Added new examples.

--- src/main/java/scaler/arrays/interviewproblems/AQ2ChristmasTrees.java
+++ src/main/java/scaler/arrays/interviewproblems/AQ2ChristmasTrees.java
@@ -53,10 +53,52 @@ public class AQ2ChristmasTrees {
 //
 //    We can choose the trees with indices 1, 2 and 3, and the cost is 1 + 2 + 3 = 6.
     public static void main(String[] args) {
-
+        int[] input1A = {1, 3, 5};
+        int[] input1B = {1, 2, 3};
+        System.out.println(new AQ2ChristmasTrees().solve(input1A, input1B));
+        int[] input2A = {1, 6, 4, 2, 6, 9};
+        int[] input2B = {2, 5, 7, 3, 2, 7};
+        System.out.println(new AQ2ChristmasTrees().solve(input2A, input2B));
+        int[] input3A = {5, 9, 10, 4, 7, 8};
+        int[] input3B = {5, 6, 4, 7, 2, 5};
+        System.out.println(new AQ2ChristmasTrees().solve(input3A, input3B));
     }
 
     public int solve(int[] A, int[] B) {
-        return 0;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < A.length - 1; i++) {
+            int middle = A[i];
+//            System.out.printf("Checking for A[%s]=%s with cost %s%n", i, A[i], B[i]);
+            int sum = B[i];
+            int j = i - 1;
+            int min = Integer.MAX_VALUE;
+//            System.out.println("1 sum : " + sum);
+            while (j >= 0) {
+                if (A[j] < middle)
+                    min = Math.min(B[j], min);
+//                System.out.printf("Min Selected from left side : B[%s]=%s%n", j, B[j]);
+                j--;
+            }
+            if (min != Integer.MAX_VALUE) {
+                sum = sum + min;
+            } else continue;
+//            System.out.println("2 sum : " + sum);
+            j = i + 1;
+            min = Integer.MAX_VALUE;
+            while (j < A.length) {
+                if (A[j] > middle)
+                    min = Math.min(B[j], min);
+//                System.out.printf("Min Selected from right side : B[%s]=%s%n", j, B[j]);
+                j++;
+            }
+            if (min != Integer.MAX_VALUE) {
+                sum = sum + min;
+            } else continue;
+//            System.out.println("3 sum : " + sum + "\n");
+            ans = Math.min(sum, ans);
+        }
+        if (ans == Integer.MAX_VALUE)
+            return -1;
+        return ans;
     }
 }
---
+++ src/main/java/scaler/arrays/interviewproblems/HWQ1MaximumPositivity.java
@@ -0,0 +1,101 @@
+package scaler.arrays.interviewproblems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HWQ1MaximumPositivity {
+    //    Problem Description
+//    Given an array of integers A, of size N.
+//
+//    Return the maximum size subarray of A having only non-negative elements. If there are more than one such subarray, return the one having the smallest starting index in A.
+//
+//            NOTE: It is guaranteed that an answer always exists.
+//
+//
+//
+//    Problem Constraints
+//1 <= N <= 105
+//
+//            -109 <= A[i] <= 109
+//
+//
+//
+//    Input Format
+//    The first and only argument given is the integer array A.
+//
+//
+//
+//            Output Format
+//    Return maximum size subarray of A having only non-negative elements. If there are more than one such subarrays, return the one having earliest starting index in A.
+//
+//
+//
+//    Example Input
+//    Input 1:
+//
+//    A = [5, 6, -1, 7, 8]
+//    Input 2:
+//
+//    A = [1, 2, 3, 4, 5, 6]
+//
+//
+//    Example Output
+//    Output 1:
+//
+//            [5, 6]
+//    Output 2:
+//
+//            [1, 2, 3, 4, 5, 6]
+//
+//
+//    Example Explanation
+//    Explanation 1:
+//
+//    There are two subarrays of size 2 having only non-negative elements.
+//            1. [5, 6]  starting point  = 0
+// 2. [7, 8]  starting point  = 3
+//    As starting point of 1 is smaller, return [5, 6]
+//    Explanation 2:
+//
+//    There is only one subarray of size 6 having only non-negative elements:
+//            [1, 2, 3, 4, 5, 6]
+    public static void main(String[] args) {
+        int[] input1A = {5, 6, -1, 7, 8};
+        System.out.println(Arrays.toString(new HWQ1MaximumPositivity().solve(input1A)));
+        int[] input2A = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(new HWQ1MaximumPositivity().solve(input2A)));
+
+    }
+
+    public int[] solve(int[] A) {
+        int finalStart = 0;
+        int finalEnd = -1;
+        int start = 0;
+        int end = -1;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] >= 0) {
+                end++;
+            } else {
+                if (end - start + 1 > finalEnd - finalStart + 1) {
+                    finalEnd = end;
+                    finalStart = start;
+                }
+                while (i < A.length && A[i] < 0) {
+                    i++;
+                }
+                end = i;
+                start = i;
+            }
+        }
+        if (end - start + 1 > finalEnd - finalStart + 1) {
+            finalEnd = end;
+            finalStart = start;
+        }
+        for (int i = finalStart; i <= finalEnd; i++) {
+            list.add(A[i]);
+        }
+        return list.stream().mapToInt(value -> value).toArray();
+    }
+}
