b9452af2182cc7ebb9870ab112c498ef426b1319
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-08-11 09:08:45 +0530

    Added new examples.

--- src/main/java/scaler/module1/hashing/AQ1CommonElements.java
+++ src/main/java/scaler/module1/hashing/AQ1CommonElements.java
@@ -1,7 +1,67 @@
 package scaler.module1.hashing;
 
-public class AQ1CommonElements {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class AQ1CommonElements {
+    //    Problem Description
+//    Given two integer arrays, A and B of size N and M, respectively. Your task is to find all the common elements in both the array.
+//            NOTE:
+//    Each element in the result should appear as many times as it appears in both arrays.
+//    The result can be in any order.
+//            Problem Constraints
+//1 <= N, M <= 105
+//            1 <= A[i] <= 109
+//    Input Format
+//    First argument is an integer array A of size N.
+//    Second argument is an integer array B of size M.
+//    Output Format
+//    Return an integer array denoting the common elements.
+//    Example Input
+//    Input 1:
+//    A = [1, 2, 2, 1]
+//    B = [2, 3, 1, 2]
+//    Input 2:
+//    A = [2, 1, 4, 10]
+//    B = [3, 6, 2, 10, 10]
+//    Example Output
+//    Output 1:
+//            [1, 2, 2]
+//    Output 2:
+//            [2, 10]
+//    Example Explanation
+//    Explanation 1:
+//    Elements (1, 2, 2) appears in both the array. Note 2 appears twice in both the array.
+//    Explantion 2:
+//    Elements (2, 10) appears in both the array.
+    public static void main(String[] args) {
+        int[] input1A = {1, 2, 2, 1};
+        int[] input1B = {2, 3, 1, 2};
+        System.out.println(Arrays.toString(new AQ1CommonElements().solve(input1A, input1B)));
+        int[] input2A = {2, 1, 4, 10};
+        int[] input2B = {3, 6, 2, 10, 10};
+        System.out.println(Arrays.toString(new AQ1CommonElements().solve(input2A, input2B)));
+    }
+
+    public int[] solve(int[] A, int[] B) {
+        Map<Integer, Integer> mapA = new HashMap<>();
+        Map<Integer, Integer> mapB = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            mapA.put(A[i], mapA.getOrDefault(A[i], 0) + 1);
+        }
+//        System.out.println(mapA);
+        for (int i = 0; i < B.length; i++) {
+            mapB.put(B[i], mapB.getOrDefault(B[i], 0) + 1);
+        }
+//        System.out.println(mapB);
+        List<Integer> list = new ArrayList<>();
+        mapA.forEach((key, value) -> {
+            if (mapB.containsKey(key)) {
+                int count = Math.min(mapB.get(key), value);
+                for (int i = 0; i < count; i++) {
+                    list.add(key);
+                }
+            }
+        });
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
---
+++ src/main/java/scaler/module1/hashing/HWQ2CheckPalindromeII.java
@@ -0,0 +1,67 @@
+package scaler.module1.hashing;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HWQ2CheckPalindromeII {
+    //    Problem Description
+//    Given a string A consisting of lowercase characters.
+//    Check if characters of the given string can be rearranged to form a palindrome.
+//    Return 1 if it is possible to rearrange the characters of the string A such that it becomes a palindrome else return 0.
+//    Problem Constraints
+//    1 <= |A| <= 105
+//    A consists only of lower-case characters.
+//            Input Format
+//    First argument is an string A.
+//    Output Format
+//    Return 1 if it is possible to rearrange the characters of the string A such that it becomes a palindrome else return 0.
+//    Example Input
+//    Input 1:
+//    A = "abcde"
+//    Input 2:
+//    A = "abbaee"
+//    Example Output
+//    Output 1:
+//            0
+//    Output 2:
+//            1
+//    Example Explanation
+//    Explanation 1:
+//    No possible rearrangement to make the string palindrome.
+//    Explanation 2:
+//    Given string "abbaee" can be rearranged to "aebbea" to form a palindrome.
+    public int solve(String A) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length(); i++) {
+            map.put(A.charAt(i), map.getOrDefault(A.charAt(i), 0) + 1);
+        }
+//        System.out.println(map);
+        if (A.length() % 2 == 0) {
+            //every character should have even frequency for even length palindrome
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                if (entry.getValue() % 2 != 0) {
+                    return 0;
+                }
+            }
+            return 1;
+        } else {
+            //every character should have even frequency for even length palindrome except for 1 odd length palindrome
+            int countOfOdd = 0;
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                if (entry.getValue() % 2 != 0) {
+                    countOfOdd++;
+                }
+            }
+            if (countOfOdd > 1) {
+                return 0;
+            }
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HWQ2CheckPalindromeII().solve("abcde"));
+        System.out.println(new HWQ2CheckPalindromeII().solve("abbaee"));
+    }
+}
