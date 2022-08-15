0877ee2c7c0c9cc4fe32410f6fa2c0de9a67f1ca
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-08-15 21:15:00 +0530

    Added new examples.

--- src/main/java/scaler/module1/recursion1/AQ2FindFibonacciII.java
+++ src/main/java/scaler/module1/recursion1/AQ2FindFibonacciII.java
@@ -37,7 +37,8 @@ public class AQ2FindFibonacciII {
     }
 
     public int findAthFibonacci(int A) {
-
-        return A;
+        if (A == 0 || A == 1)
+            return A;
+        return findAthFibonacci(A - 1) + findAthFibonacci(A - 2);
     }
 }
