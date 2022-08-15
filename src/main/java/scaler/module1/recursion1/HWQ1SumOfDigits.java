aa06e27b7577ca82be8973db4ed4818b16afc1cd
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-08-15 22:18:14 +0530

    Added new examples.

---
+++ src/main/java/scaler/module1/recursion1/HWQ1SumOfDigits.java
@@ -0,0 +1,38 @@
+package scaler.module1.recursion1;
+
+public class HWQ1SumOfDigits {
+//    Problem Description
+//    Given a number A, we need to find the sum of its digits using recursion.
+//    Problem Constraints
+//1 <= A <= 109
+//    Input Format
+//    The first and only argument is an integer A.
+//            Output Format
+//    Return an integer denoting the sum of digits of the number A.
+//    Example Input
+//    Input 1:
+//    A = 46
+//    Input 2:
+//    A = 11
+//    Example Output
+//    Output 1:
+//            10
+//    Output 2:
+//            2
+//    Example Explanation
+//    Explanation 1:
+//    Sum of digits of 46 = 4 + 6 = 10
+//    Explanation 2:
+//    Sum of digits of 11 = 1 + 1 = 2
+
+    public int solve(int A) {
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int input1A = 46;
+        System.out.println(new HWQ1SumOfDigits().solve(input1A));
+        int input2A = 11;
+        System.out.println(new HWQ1SumOfDigits().solve(input2A));
+    }
+}
