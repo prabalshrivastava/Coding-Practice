ffe80f52f9e152bd99bca993d60c4e7022f74885
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-08-23 18:15:00 +0530

    Added new examples.

---
+++ src/main/java/scaler/module1/subsequencesAndSubsets/AQ1LittlePonnyAnd2Subsequence.java
@@ -0,0 +1,36 @@
+package scaler.module1.subsequencesAndSubsets;
+
+public class AQ1LittlePonnyAnd2Subsequence {
+    //    Problem Description
+//    Little Ponny has been given a string A, and he wants to find out the lexicographically minimum subsequence from it of size >= 2. Can you help him?
+//    A string a is lexicographically smaller than string b, if the first different letter in a and b is smaller in a. For example, "abc" is lexicographically smaller than "acc" because the first different letter is 'b' and 'c' which is smaller in "abc".
+//    Problem Constraints
+//1 <= |A| <= 105
+//    A only contains lowercase alphabets.
+//            Input Format
+//    The first and the only argument of input contains the string, A.
+//            Output Format
+//    Return a string representing the answer.
+//    Example Input
+//    Input 1:
+//    A = "abcdsfhjagj"
+//    Input 2:
+//    A = "ksdjgha"
+//    Example Output
+//    Output 1:
+//            "aa"
+//    Output 2:
+//            "da"
+//    Example Explanation
+//    Explanation 1:
+//            "aa" is the lexicographically minimum subsequence from A.
+//            Explanation 2:
+//            "da" is the lexicographically minimum subsequence from A.
+    public String solve(String A) {
+        return A;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
