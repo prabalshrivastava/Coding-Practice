41babe07b3391fe1657d8243b8a92b50ac4d3069
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-08-23 11:44:20 +0530

    Added new examples.

--- src/main/java/scaler/module1/hashing2/AQ1SubarrayWithGivenSum.java
+++ src/main/java/scaler/module1/hashing2/AQ1SubarrayWithGivenSum.java
@@ -34,20 +34,21 @@ public class AQ1SubarrayWithGivenSum {
 //    Explanation 2:
 //    No subarray sums up to required number.z
 
-    public int[] solve(int[] A, int B) {
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-        map.put(0, -1);
-        int sum = 0;
+    public int[] solve1(int[] A, int B) {
+        Map<Long, Integer> map = new LinkedHashMap<>();
+        map.put(0L, -1);
+        long sum = 0;
         for (int i = 0; i < A.length; i++) {
             sum = sum + A[i];
-            map.put(sum, i);
+            if (!map.containsKey(sum))
+                map.put(sum, i);
         }
-        System.out.println(map);
+//        System.out.println(map);
         int count = -1;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            System.out.println(entry);
+        for (Map.Entry<Long, Integer> entry : map.entrySet()) {
+//            System.out.println(entry);
             if (map.containsKey(entry.getKey() - B)) {
-                System.out.printf("entry.getKey() - B : %s - %s = %s%n", entry.getKey(), B, entry.getKey() - B);
+//                System.out.printf("entry.getKey() - B : %s - %s = %s%n", entry.getKey(), B, entry.getKey() - B);
                 int start = map.get(entry.getKey() - B) + 1;
 //                System.out.printf("%s %s %n", start, count);
 //                System.out.printf("%s %s %n", start, end);
@@ -60,6 +61,33 @@ public class AQ1SubarrayWithGivenSum {
             count++;
         }
         return new int[]{-1};
+    }
+
+    public int[] solve(int[] A, int B) {
+        Map<Long, Integer> map = new LinkedHashMap<>();
+        map.put(0L, -1);
+        long sum = 0;
+        int count = 0;
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MAX_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            sum = sum + A[i];
+            if (map.containsKey(sum - B)) {
+                start = map.get(sum - B) + 1;
+                end = count;
+                break;
+            }
+            if (!map.containsKey(sum))
+                map.put(sum, i);
+            count++;
+        }
+        if (start == Integer.MAX_VALUE || end == Integer.MAX_VALUE)
+            return new int[]{-1};
+        List<Integer> ans = new ArrayList<>();
+        for (int j = start; j <= end; j++) {
+            ans.add(A[j]);
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public int[] solvePfSumOn2(int[] A, int B) {
@@ -95,16 +123,16 @@ public class AQ1SubarrayWithGivenSum {
     }
 
     public static void main(String[] args) {
-//        int[] input1A = {1, 2, 3, 4, 5};
-//        int input1B = 5;
-//        System.out.println(Arrays.toString(new AQ1SubarrayWithGivenSum().solve(input1A, input1B)));
-//
-//        int[] input2A = {5, 10, 20, 100, 105};
-//        int input2B = 110;
-//        System.out.println(Arrays.toString(new AQ1SubarrayWithGivenSum().solve(input2A, input2B)));
+        int[] input1A = {1, 2, 3, 4, 5};
+        int input1B = 5;
+        System.out.println(Arrays.toString(new AQ1SubarrayWithGivenSum().solve1(input1A, input1B)));
+
+        int[] input2A = {5, 10, 20, 100, 105};
+        int input2B = 110;
+        System.out.println(Arrays.toString(new AQ1SubarrayWithGivenSum().solve1(input2A, input2B)));
 
         int[] input3A = {42, 9, 38, 36, 48, 33, 36, 50, 38, 8, 13, 37, 33, 38, 17, 25, 50, 50, 41, 29, 34, 18, 16, 6, 49, 16, 21, 29, 41, 7, 37, 14, 5, 30, 35, 26, 38, 35, 9, 36, 34, 39, 9, 4, 41, 40, 3, 50, 27, 17, 14, 5, 31, 42, 5, 39, 38, 38, 47, 24, 41, 5, 50, 9, 29, 14, 19, 27, 6, 23, 17, 1, 22, 38, 35, 6, 35, 41, 34, 21, 30, 45, 48, 45, 37};
         int input3B = 100;
-        System.out.println(Arrays.toString(new AQ1SubarrayWithGivenSum().solve(input3A, input3B)));
+        System.out.println(Arrays.toString(new AQ1SubarrayWithGivenSum().solve1(input3A, input3B)));
     }
 }
--- src/main/java/scaler/module1/recursion2/AQ1ImplementPowerFunction.java
+++ src/main/java/scaler/module1/recursion2/AQ1ImplementPowerFunction.java
@@ -20,11 +20,42 @@ public class AQ1ImplementPowerFunction {
 //    Example Explanation
 //23 % 3 = 8 % 3 = 2
     public int pow(int A, int B, int C) {
+        return (int) power(A, B, C);
+    }
+
+    private long powerStackOverflow(long A, long B, long C) {
+        System.out.printf("%s - %s - %s %n", A, B, C);
         if (A == 0)
             return 0;
         if (B == 0)
             return 1;
-        return (A * pow(A, B - 1, C)) % C;
+        if (A < 0)
+            return ((A + C) % C * powerStackOverflow(A, --B, C) % C) % C;
+        else
+            return (A % C * powerStackOverflow(A, --B, C) % C) % C;
+    }
+
+    private long power(long A, long B, long C) {
+//        System.out.printf("%s - %s - %s %n", A, B, C);
+        if (A == 0)
+            return 0;
+        if (B == 0)
+            return 1;
+        if (B == 1) {
+            if (A < 0)
+                return (A + C) % C;
+            else
+                return A;
+        }
+
+        long x;
+        x = power(A, B / 2, C);
+        long l = ((x % C) * (x % C)) % C;
+        if (B % 2 == 0) {
+            return l;
+        } else {
+            return (A % C * l % C) % C;
+        }
     }
 
     public static void main(String[] args) {
@@ -34,6 +65,24 @@ public class AQ1ImplementPowerFunction {
         System.out.println(new AQ1ImplementPowerFunction().pow(A1, B1, C1));
         int A2 = -1, B2 = 1, C2 = 20;
         System.out.println(new AQ1ImplementPowerFunction().pow(A2, B2, C2));
-        System.out.println(-1%20);
+//        System.out.println(-1 % 20);
+        //A%m
+        //(A+m)%m = A%m + m%m
+        //(-1 + 20)%20
+
+        int A3 = 71045970;
+        int B3 = 41535484;
+        int C3 = 64735492;
+        System.out.println(new AQ1ImplementPowerFunction().pow(A3, B3, C3));
+
+        int A4 = -1;
+        int B4 = 2;
+        int C4 = 20;
+        System.out.println(new AQ1ImplementPowerFunction().pow(A4, B4, C4)); //1
+
+        int A5 = 79161127;
+        int B5 = 99046373;
+        int C5 = 57263970;
+        System.out.println(new AQ1ImplementPowerFunction().pow(A5, B5, C5));//        The expected return value: 47168647
     }
 }
