a3b8664f5136e28711aee1c7b8a03d0f81a6602f
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-08-28 10:14:53 +0530

    Added new examples.

--- src/main/java/scaler/module1/hashing2/HWQ1IsDictionary.java
+++ src/main/java/scaler/module1/hashing2/HWQ1IsDictionary.java
@@ -1,5 +1,10 @@
 package scaler.module1.hashing2;
 
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
 public class HWQ1IsDictionary {
     //    Problem Description
 //    Surprisingly, in an alien language, they also use English lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
@@ -30,7 +35,90 @@ public class HWQ1IsDictionary {
 //    Explanation 2:
 //            "none" should be present after "no". Return 0.
     public int solve(String[] A, String B) {
-        return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < B.length(); i++) {
+            map.put(B.charAt(i), i);
+        }
+        String[] C = new String[A.length];
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i];
+        }
+        Arrays.sort(A, (o1, o2) -> {
+            int max = Math.max(o1.length(), o2.length());
+            for (int i = 0; i < max; i++) {
+                if (i >= o1.length() || i >= o2.length()) {
+                    return o1.length() - o2.length();
+                }
+                if (map.get(o1.charAt(i)) - map.get(o2.charAt(i)) == 0) {
+                    continue;
+                } else {
+                    return map.get(o1.charAt(i)) - map.get(o2.charAt(i));
+                }
+            }
+            return 0;
+        });
+        for (int i = 0; i < C.length; i++) {
+            if (!C[i].equals(A[i])) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public int solve2(String[] A, String B) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < B.length(); i++) {
+            map.put(B.charAt(i), i);
+        }
+        AtomicBoolean flag = new AtomicBoolean(true);
+        AtomicInteger index = new AtomicInteger();
+        Arrays.stream(A).sorted((o1, o2) -> {
+            int max = Math.max(o1.length(), o2.length());
+            for (int i = 0; i < max; i++) {
+                if (i >= o1.length() || i >= o2.length()) {
+                    return o1.length() - o2.length();
+                }
+                if (map.get(o1.charAt(i)) - map.get(o2.charAt(i)) == 0) {
+                    continue;
+                } else {
+                    return map.get(o1.charAt(i)) - map.get(o2.charAt(i));
+                }
+            }
+            return 0;
+        }).forEach(s -> {
+            String s1 = A[index.getAndIncrement()];
+            if (!s.equals(s1)) {
+                flag.set(false);
+            }
+        });
+        return flag.get() ? 1 : 0;
+    }
+
+    public int solve1(String[] A, String B) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < B.length(); i++) {
+            map.put(B.charAt(i), i);
+        }
+        List<String> collect = Arrays.stream(A).sorted((o1, o2) -> {
+            int max = Math.max(o1.length(), o2.length());
+            for (int i = 0; i < max; i++) {
+                if (i >= o1.length() || i >= o2.length()) {
+                    return o1.length() - o2.length();
+                }
+                if (map.get(o1.charAt(i)) - map.get(o2.charAt(i)) == 0) {
+                    continue;
+                } else {
+                    return map.get(o1.charAt(i)) - map.get(o2.charAt(i));
+                }
+            }
+            return 0;
+        }).collect(Collectors.toList());
+        for (int i = 0; i < collect.size(); i++) {
+            if (!collect.get(i).equals(A[i])) {
+                return 0;
+            }
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
