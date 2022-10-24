ff29a35972417c4a1b0a3b9260d4a878ff774766
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-10-24 18:16:42 +0530

    Added new examples.

---
+++ src/main/java/scaler/advancedDsa/linkedlist3/AQ3PalindromeList.java
@@ -0,0 +1,40 @@
+package scaler.advancedDsa.linkedlist3;
+
+import scaler.common.ListNode;
+
+public class AQ3PalindromeList {
+    //    Problem Description
+//    Given a singly linked list A, determine if it's a palindrome. Return 1 or 0, denoting if it's a palindrome or not, respectively.
+//            Problem Constraints
+//1 <= |A| <= 105
+//    Input Format
+//    The first and the only argument of input contains a pointer to the head of the given linked list.
+//            Output Format
+//    Return 0, if the linked list is not a palindrome.
+//            Return 1, if the linked list is a palindrome.
+//    Example Input
+//    Input 1:
+//    A = [1, 2, 2, 1]
+//    Input 2:
+//    A = [1, 3, 2]
+//    Example Output
+//    Output 1:
+//            1
+//    Output 2:
+//            0
+//    Example Explanation
+//    Explanation 1:
+//    The first linked list is a palindrome as [1, 2, 2, 1] is equal to its reversed form.
+//    Explanation 2:
+//    The second linked list is not a palindrom as [1, 3, 2] is not equal to [2, 3, 1].
+    public static void main(String[] args) {
+        int[] input1A = {1, 2, 2, 1};
+//        System.out.println(new HWQ3PalindromeList().lPalin());
+        int[] input2A = {1, 3, 2};
+//        System.out.println(new HWQ3PalindromeList().lPalin());
+    }
+
+    public int lPalin(ListNode A) {
+        return 0;
+    }
+}
