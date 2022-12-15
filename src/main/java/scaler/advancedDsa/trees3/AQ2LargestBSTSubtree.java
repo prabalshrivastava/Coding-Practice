2be956373a9e8e55625fbbc5ebdf4dd0d72e8a0f
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-12-15 11:28:42 +0530

    Added new examples.

--- src/main/java/scaler/advancedDsa/trees3/AQ2LargestBSTSubtree.java
+++ src/main/java/scaler/advancedDsa/trees3/AQ2LargestBSTSubtree.java
@@ -1,12 +1,7 @@
 package scaler.advancedDsa.trees3;
 
-import scaler.advancedDsa.trees1.HWQ3PostorderTraversal;
 import scaler.common.TreeUtils;
 import scaler.module1.trees.TreeNode;
-
-import java.util.AbstractMap;
-import java.util.Arrays;
-import java.util.Map;
 
 public class AQ2LargestBSTSubtree {
     //    Problem Description
@@ -53,33 +48,91 @@ public class AQ2LargestBSTSubtree {
 
         int[] input1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         System.out.println(new AQ2LargestBSTSubtree().solve(TreeUtils.mapArrayToTree(input1)));
+//        9 20 14 12 11 -1 -1 -1 -1 -1
+
     }
 
+    int max = 1;
+
     public int solve(TreeNode A) {
-        Map.Entry<Integer, Boolean> validBSTWithSize = isValidBSTWithSize(A);
-        max = Math.max(validBSTWithSize.getKey(), max);
+        if (A == null)
+            return 0;
+        postorderWithSize(A);
         return max;
     }
 
-    TreeNode previous = null;
-    int max = Integer.MIN_VALUE;
-
-    private Map.Entry<Integer, Boolean> isValidBSTWithSize(TreeNode A) {
+    private int postorderWithSize(TreeNode A) {
         if (A == null)
-            return new AbstractMap.SimpleEntry<>(0, true);
-        Map.Entry<Integer, Boolean> leftBST = isValidBSTWithSize(A.left);
-        if (leftBST.getValue())
-            max = Math.max(leftBST.getKey(), max);
-        if (previous != null && previous.val > A.val) {
-            System.out.println("----------------------");
-            return new AbstractMap.SimpleEntry<>(leftBST.getKey(), false);
+            return 0;
+        if (A.left == null && A.right == null)
+            return 1;
+        int left = postorderWithSize(A.left);
+        int right = postorderWithSize(A.right);
+        int size = left + right + 1;
+        previous = null;
+        boolean isValidLeft = isValid(A.left);
+        previous = null;
+        boolean isValidRight = isValid(A.right);
+        previous = null;
+        boolean isValidRoot = isValid(A);
+//        System.out.println("A : " + A + isValidLeft + " - " + isValidRight + " - " + isValidRoot);
+        if (isValidLeft && isValidRight && isValidRoot) {
+//            System.out.println("A : " + A + "->" + size);
+            max = Math.max(size, max);
         }
-        previous = A;
-        Map.Entry<Integer, Boolean> rightBST = isValidBSTWithSize(A.right);
-        if (rightBST.getValue())
-            max = Math.max(rightBST.getKey(), max);
-
-        if (leftBST.getValue() && rightBST.getValue()) max = Math.max(leftBST.getKey() + rightBST.getKey() + 1, max);
-        return new AbstractMap.SimpleEntry<>(leftBST.getKey() + rightBST.getKey() + 1, leftBST.getValue() && rightBST.getValue());
+        return size;
     }
+
+
+    TreeNode previous = null;
+
+
+    private boolean isValid(TreeNode curr) {
+        if (curr == null)
+            return true;
+        boolean isLeftValidBST = isValid(curr.left);
+        if (previous != null && curr.val <= previous.val) {
+            return false;
+        }
+        previous = curr;
+        boolean isRightValidBST = isValid(curr.right);
+        return isRightValidBST && isLeftValidBST;
+    }
+//    public int isValidBST(TreeNode A) {
+//        return isValid(A) ? 1 : 0;
+//    }
+
+//    public int solve(TreeNode A) {
+//        Map.Entry<Integer, Boolean> validBSTWithSize = isValidBSTWithSize(A);
+//        max = Math.max(validBSTWithSize.getKey(), max);
+//        return max;
+//    }
+//
+//    TreeNode previous = null;
+//    int max = Integer.MIN_VALUE;
+//
+//    private Map.Entry<Integer, Boolean> isValidBSTWithSize(TreeNode curr) {
+//        if (curr == null)
+//            return new AbstractMap.SimpleEntry<>(0, true);
+//        if (curr.left == null && curr.right == null)
+//            return new AbstractMap.SimpleEntry<>(1, true);
+//
+//        Map.Entry<Integer, Boolean> leftBST = isValidBSTWithSize(curr.left);
+//        if (leftBST.getValue())
+//            max = Math.max(leftBST.getKey(), max);
+//
+//        if (previous != null && curr.val <= previous.val) {
+//            return new AbstractMap.SimpleEntry<>(leftBST.getKey(), false);
+//        }
+//        previous = curr;
+//
+//        Map.Entry<Integer, Boolean> rightBST = isValidBSTWithSize(curr.right);
+//        System.out.println("leftBST:" + curr.left + "-" +  leftBST + " | rightBST:" + curr.right + " - " +  rightBST);
+//        if (rightBST.getValue())
+//            max = Math.max(rightBST.getKey(), max);
+//
+//        if (leftBST.getValue() && rightBST.getValue())
+//            max = Math.max(leftBST.getKey() + rightBST.getKey() + 1, max);
+//        return new AbstractMap.SimpleEntry<>(leftBST.getKey() + rightBST.getKey() + 1, leftBST.getValue() && rightBST.getValue());
+//    }
 }
