235e3f17195cfa9d8c8fe16780ba0b669d0ed282
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2022-12-20 10:20:49 +0530

    Added new examples.

--- src/main/java/scaler/advancedDsa/queueanddeque/AQ2SlidingWindowMaximum.java
+++ src/main/java/scaler/advancedDsa/queueanddeque/AQ2SlidingWindowMaximum.java
@@ -1,7 +1,8 @@
 package scaler.advancedDsa.queueanddeque;
 
 import java.awt.*;
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.DelayQueue;
 
 public class AQ2SlidingWindowMaximum {
 //    Problem Description
@@ -53,9 +54,41 @@ public class AQ2SlidingWindowMaximum {
         int[] input2A = {1, 2, 3, 4, 2, 7, 1, 3, 6};
         int input2B = 6;
         System.out.println(Arrays.toString(new AQ2SlidingWindowMaximum().slidingMaximum(input2A, input2B)));
+        int[] input3A = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int input3B = 2;
+        System.out.println(Arrays.toString(new AQ2SlidingWindowMaximum().slidingMaximum(input3A, input3B)));//10 9 8 7 6 5 4 3 2
     }
 
     public int[] slidingMaximum(final int[] A, int B) {
-        return A;
+        Deque<Integer> dequeue = new ArrayDeque<>();
+        int[] ans = new int[A.length - B + 1];
+        int count = 0;
+//        int max = Integer.MIN_VALUE;
+//        for (int i = 0; i < B; i++) {
+//            max = Math.max(max,A[i]);
+//        }
+//        ans[count++] = max;
+        for (int i = 0; i < A.length; i++) {
+            if (i - B >= 0 && dequeue.peekFirst() == A[i - B]) {
+                dequeue.pollFirst();
+            }
+            Integer lastElement = dequeue.peekLast();
+            if (dequeue.isEmpty()) {
+                dequeue.offerLast(A[i]);
+            } else {
+                if (A[i] < lastElement) {
+                    dequeue.offerLast(A[i]);
+                } else {
+                    while (!dequeue.isEmpty() && dequeue.peekLast() < A[i]) {
+                        dequeue.pollLast();
+                    }
+                    dequeue.offerLast(A[i]);
+                }
+            }
+            if (i + 1 >= B) {
+                ans[count++] = dequeue.peekFirst();
+            }
+        }
+        return ans;
     }
 }
