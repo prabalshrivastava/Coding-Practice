d8947d9ca4010265664a6a4db6d196a845f5c254
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2023-06-20 11:02:33 +0530

    Added new examples.

--- src/main/java/scaler/advancedDsa/arrays2/AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix.java
+++ src/main/java/scaler/advancedDsa/arrays2/AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix.java
@@ -1,7 +1,10 @@
 package scaler.advancedDsa.arrays2;
 
+import java.util.Arrays;
+
 public class AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix {
-    //    Problem Description
+
+  //    Problem Description
 //    Given a row-wise and column-wise sorted matrix A of size N * M.
 //    Return the maximum non-empty submatrix sum of this matrix.
 //            Problem Constraints
@@ -37,15 +40,44 @@ public class AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix {
 //        4 5 6
 //        7 8 9
 //    The sum is 45.
-    public static void main(String[] args) {
-        int[][] input1 = {{-5 - 4, 3}, {-1, 2, 3}, {2, 2, 4}};
-        System.out.println(new AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix().solve(input1));
-        int[][] input2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        System.out.println(new AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix().solve(input2));
-    }
+  public static void main(String[] args) {
+    int[][] input1 = {{-5, -4, 3}, {-1, 2, 3}, {2, 2, 4}};
+    System.out.println(new AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix().solve(input1));
+    int[][] input2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    System.out.println(new AQ4MaximumSubmatrixSumRowWiseAndColumnWiseSortedMatrix().solve(input2));
+  }
 
-    public long solve(int[][] A) {
-
-        return 0;
+  public long solve(int[][] A) {
+    for (int i = 0; i < A.length; i++) {
+      int prefix = 0;
+      for (int j = 0; j < A[i].length; j++) {
+        A[i][j] = A[i][j] + prefix;
+        prefix = A[i][j];
+      }
     }
+    System.out.println(Arrays.deepToString(A));
+    for (int j = 0; j < A[0].length; j++) {
+      int prefix = 0;
+      for (int i = 0; i < A.length; i++) {
+        A[i][j] = A[i][j] + prefix;
+        prefix = A[i][j];
+      }
+    }
+    System.out.println(Arrays.deepToString(A));
+
+    for (int si = 0; si < A.length; si++) {
+      for (int sj = 0; sj < A[si].length; sj++) {
+//        System.out.printf("[%s][%s] -> ", si, sj);
+        long maxSum = 0;
+        for (int ei = si; ei < A.length; ei++) {
+          for (int ej = sj; ej < A[ei].length; ej++) {
+//            System.out.printf("\t[%s][%s]", ei, ej);
+
+          }
+        }
+//        System.out.println();
+      }
+    }
+    return 0;
+  }
 }
